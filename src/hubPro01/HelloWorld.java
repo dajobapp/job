ddff0febfe84a60c5903b25da5dcfb08ba1c5462
김민성 <dajobapps@gmail.com>
2020-04-09 23:46:07 +0900

    lll

--- src/hubPro01/HelloWorld.java
+++ src/hubPro01/HelloWorld.java
@@ -3,23 +3,8 @@ package hubPro01;
 public class HelloWorld {
 	public static void main(String[] args) {
 		//test 2
-		
-		// test �뜝�뙃�땲�뙋�삕. test 23  5656
-		  //335533         
-		//121212
-		//as/fas/ sdfsadfsafa  jjj
-		//test ttt
-		
-		//asfsdfsadf
-		//2222
-		//sfsdfsdf  �뜝�룞�삕�뜝�룞�삕�뜝�룞�삕
-		//asf �뜝�룞�삕�뜝�룞�삕�뜝�룞�삕
-		///�뜝�룞�삕�뜝�룞�삕�뜝�룞�삕 kkk   占쎈�ㅿ옙�ㅿ옙�� 占쎈�わ옙�わ옙�챱=uuu
-		///nnn
-		
-		//ttt   rr bbb �뀕�뀕�뀕
-		
-		hjg
+	
+		//hjg
 	}
 
 }
